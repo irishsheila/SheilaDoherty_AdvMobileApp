4bef95f1be80f0bbb898b784a11cdf3c86eca696
Sheila <Sheila@Serenity.local>
2018-02-07 15:06:42 -0700

    Upload Lab1

--- Lab1/Alternative/Alternative/ThirdViewController.swift
+++ Lab1/Alternative/Alternative/ThirdViewController.swift
@@ -10,17 +10,62 @@ import UIKit
 import AVFoundation
 
 class ThirdViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
-
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     
     var audioPlayer: AVAudioPlayer?
     var audioRecorder: AVAudioRecorder?
     let fileName = "audio.m4a"
+    
+    @IBAction func recordAudio(_ sender: UIButton) {
+        if audioRecorder?.isRecording == false{
+            playButton.isEnabled = false
+            stopButton.isEnabled = true
+            audioRecorder?.delegate = self
+            print("start recording")
+            audioRecorder?.record()
+        }
+    }
+    
+    @IBAction func playAudio(_ sender: UIButton) {
+        if audioRecorder?.isRecording == false{
+            stopButton.isEnabled = true
+            recordButton.isEnabled = false
+            
+            do {
+                try audioPlayer = AVAudioPlayer(contentsOf:
+                    (audioRecorder?.url)!)
+                audioPlayer!.delegate = self
+                audioPlayer!.prepareToPlay()
+                print("start playing")
+                audioPlayer!.play()
+            } catch let error as NSError {
+                print("audioPlayer error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func stopAudio(_ sender: UIButton) {
+        stopButton.isEnabled = false
+        playButton.isEnabled = true
+        recordButton.isEnabled = true
+        if audioRecorder?.isRecording == true {
+            print("stop recording")
+            audioRecorder?.stop()
+        } else {
+            print("stop playing")
+            audioPlayer?.stop()
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //playButton.isEnabled = false
-        //stopButton.isEnabled = false
+
+        playButton.isEnabled = false;
+        stopButton.isEnabled = false;
         
         let dirPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let docDir = dirPath[0]
@@ -28,19 +73,20 @@ class ThirdViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecor
         print(audioFileURL)
         
         let audioSession = AVAudioSession.sharedInstance()
-        do{
+        do {
             try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
         } catch {
-            print("audio seesion error: \(error.localizedDescription)")
+            print("audio session error: \(error.localizedDescription)")
         }
+        
         let settings = [
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
             AVSampleRateKey: 12000,
             AVNumberOfChannelsKey: 1,
             AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-            ]
+        ]
         
-        do{
+        do {
             audioRecorder = try AVAudioRecorder(url: audioFileURL, settings: settings)
             audioRecorder?.prepareToRecord()
             print("audio recorder ready")
@@ -48,7 +94,12 @@ class ThirdViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecor
             print("audio recorder error: \(error.localizedDescription)")
         }
     }
-
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        recordButton.isEnabled = true
+        stopButton.isEnabled = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
